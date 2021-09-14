a80e8f9dd699d7562efdc2c5f3b1cd3525d28503
Brian Diaz <bidiaz101@gmail.com>
2021-09-14 14:02:11 -0500

    commit tree 1cd6b8148e3ba4bc1f93bcffbd051e1c1270e816

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,22 @@
 // create your App component here
+import React, { useEffect, useState } from "react"
+
+function App() {
+    const [imgUrl, setImgUrl] = useState("")
+
+    useEffect(() => {
+        fetch("https://dog.ceo/api/breeds/image/random")
+        .then(resp => resp.json())
+        .then(data => {
+            setImgUrl(data.message)
+        })
+    }, [])
+    
+    if(!imgUrl){
+        return <p>Loading...</p>
+    } else {
+        return <img src={imgUrl} alt="A Random Dog" />
+    }
+}
+
+export default App
